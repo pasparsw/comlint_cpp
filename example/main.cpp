e14bcd9eddf10634312918d2121ee569b6065720
pawbar94 <paw.bar94@gmail.com>
2023-01-30 15:55:00 +0100

    Extended example

--- example/main.cpp
+++ example/main.cpp
@@ -44,44 +44,49 @@ void run_merge_command(const ParsedCommand &command)
     }
 }
 
-void run_status_command(const ParsedCommand &command)
+void run_submodule_command(const ParsedCommand &command)
 {
-    std::cout << "Running status command!" << std::endl;
+    std::cout << "Running submodule " << command.values.front() << " command!" << std::endl;
 
-    if (command.IsOptionUsed("-b")) {
-        std::cout << "Showing status for branch " << command.options.at("-b") << std::endl;
-    }
     if (command.flags.at("--verbose")) {
         std::cout << "Being verbose" << std::endl;
-    }
-    if (command.flags.at("--short")) {
-        std::cout << "Showing short output" << std::endl;
     }
 }
 
 int main(int argc, char** argv)
 {
     try {
+        // create a command line interface which always requires command line arguments
         CommandLineInterface cli(argc, argv, "ExampleApplication", "Example usage of Comlint library basing on some git commands", false);
 
-        // add "add" command which requires 1 value (it )
-        cli.AddCommand("add", "Add files to commit", 1U, ANY, ANY, {"--verbose", "--interactive"});
+        // add "add" command which requires 1 value (it can be any value), has no allowed options and 2 allowed flags (no required options by default)
+        cli.AddCommand("add", "Add files to commit", 1U, ANY, NONE, {"--verbose", "--interactive"});
+        // add "commit" command which does not require any value, has 2 allowed options and 2 allowed flags (no required options by default)
         cli.AddCommand("commit", "Commit changes", {"-m", "-c"}, {"--verbose", "--amend"});
-        cli.AddCommand("merge", "Merge two branches", 2U, ANY, {"-s", "-m"}, {}, {"-s"});
-        cli.AddCommand("status", "Show current status", {"-b"}, {"--verbose", "--short"});
+        // add "merge" command which requires 2 values (these can be any values), has 2 allowed option, no allowed flags and 1 required options
+        cli.AddCommand("merge", "Merge two branches", 2U, ANY, {"-s", "-m"}, NONE, {"-s"});
+        // add "submodule command which requires 1 value (it can be either "add" or "update"), has no allowed options and 1 allowed flag (no required options by default)
+        cli.AddCommand("submodule", "Perform operation on submodule", 1U, {"add", "update"}, NONE, {"--verbose"});
 
+        // add option "-b" (which can take any value by default)
         cli.AddOption("-b", "Specify branch name");
+        // add option "-b" (which can take any value by default)
         cli.AddOption("-m", "Provide message");
+        // add option "-b" (which can take any value by default)
         cli.AddOption("-c", "Provide commit hash");
+        // add option "-b" which can take one of three allowed values: recursive, resolve or subtree
         cli.AddOption("-s", "Specify merging strategy", {"recursive", "resolve", "subtree"});
 
+        // add flags
         cli.AddFlag("--verbose", "Show verbose output");
         cli.AddFlag("--short", "Show short status output");
         cli.AddFlag("--interactive", "Add files to commit interactively");
         cli.AddFlag("--amend", "Join to previous commit");
 
+        // obtain parsed command with its properties
         const ParsedCommand parsed_command = cli.Parse();
 
+        // use parsed command to perform actions
         if (parsed_command.name == "add") {
             run_add_command(parsed_command);
         }
@@ -91,8 +96,8 @@ int main(int argc, char** argv)
         if (parsed_command.name == "merge") {
             run_merge_command(parsed_command);
         }
-        if (parsed_command.name == "status") {
-            run_status_command(parsed_command);
+        if (parsed_command.name == "submodule") {
+            run_submodule_command(parsed_command);
         }
     }
     catch (const std::exception &e) {
